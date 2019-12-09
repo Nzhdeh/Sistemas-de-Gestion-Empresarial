9eb3632401a1fe2c0601112addae1c710d7d458c
nzhdeh21 <nzhdeh1991@gmail.com>
2019-12-09 15:44:39 +0100

    esqueleto del examen

---
+++ Examen1TrimestreNzhdeh/Examen1TrimestreNzhdeh-ET/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Examen1TrimestreNzhdeh_ET
+{
+    public class Class1
+    {
+    }
+}
---
+++ Examen1TrimestreNzhdeh/Examen1TrimestreNzhdeh-UI/Models/ClsClasesConNombresLargos.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Examen1TrimestreNzhdeh_UI.Models
+{
+    public class ClsClasesConNombresLargos
+    {
+
+    }
+}
