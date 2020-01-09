4ee3a1b5b1ea8886ce541fe23eb7eadded6598ac
Nzhdeh <nzhdeh1991@gmail.com>
2020-01-09 14:41:26 +0100

    continuará

---
+++ 12-HolaMundoJS/12-HolaMundoJS/Js/HolaMundoJs.js
@@ -0,0 +1,18 @@
+﻿window.onload = inicializar;//aqui no hay que poner los parentesis,
+                            //porque esto es para lo que quiero que haga cunado se cargue la ventana
+
+//funcion que inicializa los elementos de la pagina
+function inicializar() {
+    //alert("Hola mundo");
+    //añadir evento click al boton Pulsar y que llame al evento
+    //cambiarTexto();
+
+    document.getElementById("btnPulsar").addEventListener("click", cambiarTexto, false);
+}
+
+//cambia el texto del div divTexto
+function cambiarTexto()
+{
+    //alert("Hola mundo");
+    document.getElementById("divTexto").innerHTML = "Hola mundo";
+}
---
+++ 13-HolaMundoMejoradoJs/13-HolaMundoMejoradoJs/Js/ClsPersona.js
@@ -0,0 +1,8 @@
+﻿class Persona
+{
+    constructor Persona(nombre, apellidos)
+    {
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+    }
+}
